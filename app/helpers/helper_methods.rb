d48e10f00c03cd817251016461f0c47588cbe7d6
gvien <wavetech86@gmail.com>
2015-08-11 20:13:25 -0700

    add helper method to render the stock info page

--- app/helpers/helper_methods.rb
+++ app/helpers/helper_methods.rb
@@ -14,7 +14,22 @@ helpers do
 	end
 
 	def dataExist?(symbol_field)
-		Stock.new.data(symbol_field[:sym], symbol_field[:period].to_i).length > 0
+		YahooFinanceDataCollector.run(symbol_field[:sym], symbol_field[:period].to_i).length > 0
 	end
 
+	def render_page_with_stock_info(symbol_field)
+		if dataExist?(symbol_field) && request.xhr?
+			@comment = Comment.all
+			@data = YahooFinanceDataCollector.run(symbol_field[:sym], symbol_field[:period].to_i)
+			@stock = Stock.find_or_create_by(symbol: symbol_field[:sym])
+			erb :_field, layout: false
+		elsif !dataExist?(symbol_field) && request.xhr?
+			@symbol_error = "<p id=\"invalid-sym\">The symbol <span class=\"sym\">#{symbol_field[:sym]}</span> is not valid. Try a different one, e.g. GOOG, YHOO, APPL, etc.</p>"
+			# erb :"_invalid-symbol", layouot: false	#no need for this, since it adds it in the body for no reasons
+			content_type :json
+			{symbol_error: @symbol_error}.to_json
+		else
+			redirect "/stocks/#{symbol_field[:sym]}/period/#{symbol_field[:period]}"	#/users/#{params[:user_id]}/#{params[:symbol]}/#{params[:period]}"
+		end
+	end
 end
